e6f523d45a64fb0660c041635aec7e08465e3f79
Varun Urrinkala <balavarunkumar.u@gmail.com>
2026-06-17 18:21:52 +0530

    first commit

---
+++ Task1.js
@@ -0,0 +1,27 @@
+var EmployeeName = "Varun";
+let age = 22;
+const companyName ="Stackly";
+
+console.log("Employee Name: " + EmployeeName);
+console.log("Employee Age: " + age);
+console.log("Company Name: " + companyName);
+
+alert("Welcome to Employee Portal");
+
+let TaskCompleted = confirm("Have you completed today's task?");
+console.log(TaskCompleted);
+
+let employeeName = prompt("Enter your name:");
+console.log(employeeName);
+
+document.write("Welcome " + employeeName);
+
+console.warn("Task submitted successfully");
+
+console.error("Task submission failed");
+
+age = 23;
+
+console.log("Updated Employee Age: " + age);
+
+
